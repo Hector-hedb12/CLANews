eda89e9b3114bb0055a5030ffc7847d8fc979a7f
Hector <hedb_12@hotmail.com>
2014-11-26 12:58:59 -0430

    precision-recall chart and test tweet intent

--- Classifier/src/classifier/Classifier.java
+++ Classifier/src/classifier/Classifier.java
@@ -2,12 +2,16 @@ package classifier;
 
 import java.awt.BorderLayout;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
+import java.util.Scanner;
 import weka.classifiers.Evaluation;
 import weka.classifiers.evaluation.ThresholdCurve;
 import weka.classifiers.functions.LibSVM;
+import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instances;
+import weka.core.SparseInstance;
 import weka.core.Utils;
 import weka.core.converters.ArffLoader;
 import weka.core.converters.ArffSaver;
@@ -24,77 +28,114 @@ import weka.gui.visualize.ThresholdVisualizePanel;
  * @author Hector E. Dominguez B.
  */
 public class Classifier {
-    private static final int NUM_FOLDS = 10;
     // SVM Parameters
+    private static final int BEST_NUM_INSTANCES  = 2000;
+    private static final int BEST_NUM_ATTRIBUTES = 1500;
+    private static final int BEST_SEED           = 2000;
+    
+    private static final int    NUM_FOLDS = 10;
     private static final double SVM_COST  = 1.0;
     private static final double SVM_GAMMA = 0.25;
     
     // Processed tweets
-    private static final String DIR = "../News/Processed";
-    private static final String OUT = "src/resources/pro_SVM_input.arff";
-    // Raw tweets
-    //private static final String DIR = "../News/Raw";
-    //private static final String OUT = "src/resources/raw_SVM_input.arff";
+    private static final String DIR       = "../News/Processed";
+    private static final String ARFF_FILE = "src/resources/all_tweets.arff";
     
-    private static Instances mInstances, mStructure;
+    // Tuning parameters
+    private static final int [] numInstances_list  = new int[] {1000, // per category 
+                                                                2000, 
+                                                                3000, 
+                                                                4000};
     
-    private static final int [] numInstances_list  = new int[] {1000, 2000, 3000, 4000}; // per category
-    private static final int [] numAttributes_list = new int[] {500, 1000, 1500, 2000, 2500};
+    private static final int [] numAttributes_list = new int[] {500,
+                                                                1000, 
+                                                                1500, 
+                                                                2000, 
+                                                                2500};
     
-    private static final int BEST_NUM_INSTANCES  = 4000;
-    private static final int BEST_NUM_ATTRIBUTES = 2500;
-    private static final int BEST_SEED           = 2000;
+    // Location trained machine
+    private static final String MODEL   = "src/resources/svm.model";
+    private static final String DATASET = "src/resources/svm_model_dataset.arff";
     
-    private static final String MODEL = "src/resources/svm.model";
+    // Members variables
     private static LibSVM mSVM;
+    private static Instances mInstances;
+    private static Instances mSVM_dataset;
+    
+    //private static final String OUT_PLOT = "src/resources/plot/ROC_SVM";
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        ArffLoader loader = new ArffLoader();
-        try {
-            loader.setFile( new File(OUT) );
-            mInstances = loader.getDataSet();
-        } catch (NullPointerException ex) { // File not found
-            if ( mInstances == null ) {
-                System.out.println("Reading Tweets from " + DIR + " directory...");
-                fromTweetsToArff(DIR, OUT);
-            }
-        }
         
-        mInstances.setClassIndex(0);
+        ArffLoader loader = new ArffLoader();
         
         // Tuning data parameters
         //getBestDataParameters();
-        
+                
         mSVM = getModel(); // try get model
         
         if ( mSVM == null) {
-            saveModel(doCrossValidation(BEST_SEED,
-                                        getTrainingSet(BEST_NUM_INSTANCES, 
-                                                       BEST_NUM_ATTRIBUTES))
-            );
+            // Load all tweets
+            try {
+                loader.setFile( new File(ARFF_FILE) );
+                mInstances = loader.getDataSet();
+                System.out.println("Loading ARFF File...");
+            } catch (NullPointerException ex) { // File not found
+                if ( mInstances == null ) {
+                    fromTweetsToArff(DIR, ARFF_FILE);
+                }
+            }
+
+            mInstances.setClassIndex(0);
+            
+            // Get sample of all tweets
+            Instances dataset = getTrainingSet(BEST_NUM_INSTANCES, 
+                                               BEST_NUM_ATTRIBUTES);
+            
+            dataset.setRelationName("svm_model_dataset");
+            // Train and Save SVM
+            saveModel( doCrossValidation(BEST_SEED, dataset) );
+            // Save dataset used to train SVM
+            ArffSaver saver = new ArffSaver();
+            saver.setInstances(dataset);
+            saver.setFile( new File(DATASET) );
+            saver.writeBatch();
+        } else {
+            // Load dataset used to train SVM
+            try {
+                loader.setFile( new File(DATASET) );
+                mSVM_dataset = loader.getDataSet();
+            } catch (Exception ex) { // File not found
+                System.err.println("Loading ARFF File: " + DATASET);
+                System.exit(-1);            
+            }
+
+            mSVM_dataset.setClassIndex(0);
         }
+        
+        //test("../News/Test/tweets.txt");
     }
     
     private static void fromTweetsToArff(String fromDir, String outputFile) 
             throws Exception {
         
+        System.out.println("Reading Tweets from directory:" + fromDir );
+        
+        System.out.println("\tLoading...");
         TextDirectoryLoader loader = new TextDirectoryLoader();
         loader.setDirectory( new File(fromDir) );
         Instances rawData = loader.getDataSet();
-        //System.out.println("\n\nRaw data:\n\n" + rawData);
-                
+        
+        System.out.println("\tFiltering...");
         StringToWordVector filter = new StringToWordVector();
         filter.setInputFormat(rawData);
-        //filter.setOutputWordCounts(true);
-        //filter.setWordsToKeep(1000000);
-        //filter.setDoNotOperateOnPerClassBasis(true);
+        filter.setOutputWordCounts(true);
         mInstances = Filter.useFilter(rawData, filter);
         mInstances.setRelationName("news");
-        //System.out.println("\n\nFiltered data:\n\n" + data);
         
+        System.out.println("\tSaving...");
         ArffSaver saver = new ArffSaver();
         saver.setInstances(mInstances);
         saver.setFile( new File(outputFile) );
@@ -160,7 +201,7 @@ public class Classifier {
                                 "-W","1",
                                 "-K", "weka.classifiers.functions.supportVector.PolyKernel " +
                                       "-C 250007 -E 1.0"} ;
-                */
+        */
         //svm.setOptions(options);
         //svm.buildClassifier(newData);
         
@@ -170,12 +211,11 @@ public class Classifier {
         Evaluation eval, evalAll;
         double best_Fbeta; // Weighted Harmonic Mean
         
-        bestSVM   = null;
+        bestSVM    = null;
         best_Fbeta = 0.0;
         evalAll    = new Evaluation(randData);
         
         for ( int i = 0; i < NUM_FOLDS; i++ ) {
-            System.out.println("\n=== Fold " + (i+1) + "/" + NUM_FOLDS + " ===\n");
             eval  = new Evaluation(randData);
             train = randData.trainCV(NUM_FOLDS, i);
             test  = randData.testCV(NUM_FOLDS, i);
@@ -186,6 +226,8 @@ public class Classifier {
             mySVM.setCost(SVM_COST);
             mySVM.setGamma(SVM_GAMMA);
             mySVM.setSeed(seed);
+            mySVM.setProbabilityEstimates(true);
+            mySVM.setNormalize(true);
             mySVM.buildClassifier(train);
             eval.evaluateModel(mySVM, test);
             evalAll.evaluateModel(mySVM, test);
@@ -195,6 +237,12 @@ public class Classifier {
                 best_Fbeta = eval.weightedFMeasure();
                 bestSVM = (LibSVM) LibSVM.makeCopy(mySVM);
             }
+            
+            System.out.println("\n=== Fold " + (i+1) + "/" + NUM_FOLDS + 
+                               ", Harmonic Mean (F-measure): "         + 
+                               eval.weightedFMeasure() +" ===\n");
+            
+            //saveROCPlot(eval,i);
             
             /*
             // Output evaluation
@@ -217,8 +265,10 @@ public class Classifier {
         System.out.println("=== Best SVM ===");
         System.out.println("Weighted Harmonic Mean: " + best_Fbeta );
 
+        //showROCPlot(NUM_FOLDS);
+        //showRocCurve(evalAll.predictions());
+        
         return bestSVM;
-        //showRocCurve(eval.predictions());
     }
 
     private static void showRocCurve(FastVector predictions) {
@@ -308,5 +358,106 @@ public class Classifier {
         }
         
         return classifier;
+    }
+/*
+    private static void saveROCPlot(Evaluation eval, int i) throws Exception {
+        ThresholdCurve tc = new ThresholdCurve();
+        Instances result = tc.getCurve(eval.predictions(), 0);
+        ArffSaver saver = new ArffSaver();
+        
+        saver.setInstances(result);
+        saver.setFile(new File(OUT_PLOT + i +".arff"));
+        saver.writeBatch();
+    }
+    
+    private static void showROCPlot(int N) throws Exception {
+        boolean first = true;
+        ThresholdVisualizePanel tvp = new ThresholdVisualizePanel();
+        
+        for (int i = 0; i < N; i++) {
+            Instances curve = DataSource.read(OUT_PLOT + i +".arff");
+            curve.setClassIndex(curve.numAttributes() - 1);
+            
+            // method visualize
+            PlotData2D plotdata = new PlotData2D(curve);
+            plotdata.setPlotName("ROC_SVM "+ (i+1));
+            plotdata.addInstanceNumberAttribute();
+            
+            // specify which points are connected
+            boolean[] cp = new boolean[curve.numInstances()];
+            for (int n = 1; n < cp.length; n++)
+                cp[n] = true;
+            
+            plotdata.setConnectPoints(cp);
+            // add plot
+            
+            if (first)
+                tvp.setMasterPlot(plotdata);
+            else
+                tvp.addPlot(plotdata);
+            
+            first = false;
+        }
+     
+        //Parametros Combobox
+        tvp.setYIndex(7);
+        tvp.setXIndex(8);
+        tvp.setColourIndex(10);
+        
+        // method visualizeClassifierErrors
+        final JFrame jf = new JFrame("WEKA ROC");
+        jf.setSize(500,400);
+        jf.getContentPane().setLayout(new BorderLayout());
+        jf.getContentPane().add(tvp, BorderLayout.CENTER);
+        jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jf.setVisible(true);
+    }
+*/
+    private static void test(String file) {
+       
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File(file));
+        } catch (FileNotFoundException ex) {
+            System.err.println("File : " + file + " not found");
+            System.exit(-1);
+        }
+        
+        Instances testSet = mSVM_dataset.stringFreeStructure();
+        
+        while ( scanner.hasNextLine() ) {
+            String tweet = scanner.nextLine();
+            addInstance(tweet,testSet);
+        }
+        
+        int numTweets = testSet.numInstances();
+        for (int i = 0; i < numTweets; i++ ) {
+            try {
+                double prediction = mSVM.classifyInstance(testSet.instance(i));
+                System.out.println(testSet.classAttribute().value((int) prediction));
+            } catch (java.lang.Exception ex) {
+                System.err.println("?: " + testSet.instance(i).toString());
+            }
+        }
+    }
+    
+    private static void addInstance(String tweet, Instances data) {
+        double[] values = new double[data.numAttributes()];
+        Attribute wordAttribute;
+        Scanner scanner = new Scanner(tweet);
+        String word;
+        
+        while( scanner.hasNext() ) {
+            word = scanner.next();
+            wordAttribute = data.attribute(word);
+            if ( wordAttribute != null ) {
+                values[wordAttribute.index()] += 1.0;
+            }
+        }
+        
+        SparseInstance instance = new SparseInstance(1.0, values);
+        instance.setMissing(0);
+
+        data.add(instance);
     }
 }
